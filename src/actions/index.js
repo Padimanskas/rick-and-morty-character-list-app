48dfe876ff1cbce275fdfa6a45b540f60cc4a7dc
Stanislav Padimanskas <s.padimanskas@gmail.com>
2020-09-09 00:30:38 +0300

    actions fixed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,17 @@
-const GET_CHARACTERS = 'GET_CHARACTERS';
-const CHARACTERS_LOADED_SUCCESS = 'CHARACTERS_LOADED_SUCCESS';
+export const GET_CHARACTERS = 'GET_CHARACTERS';
+export const CHARACTERS_LOADED_SUCCESS = 'CHARACTERS_LOADED_SUCCESS';
+export const CHARACTERS_LOADED_FILED = 'CHARACTERS_LOADED_FILED';
 
 export const getCharacters = () => ({
-    type: 'GET_CHARACTERS',
+    type: GET_CHARACTERS,
 });
 
 export const charactersLoadedSuccess = (payload) => ({
-    type: 'CHARACTERS_LOADED_SUCCESS',
+    type: CHARACTERS_LOADED_SUCCESS,
     payload
+});
+
+export const charactersLoadedFiled = (error) => ({
+    type: CHARACTERS_LOADED_FILED,
+    payload: error
 });
